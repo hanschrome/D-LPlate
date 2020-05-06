d771413f72e9732e2e08c2c9e1987dcab7561c46
Hans <hanscastroadmn@gmail.com>
2020-05-06 21:43:13 +0200

    Green line to identify contours with threshold

--- run.py
+++ run.py
@@ -15,11 +15,15 @@ if len(sys.argv) == 1 or sys.argv[0] == '-h':
         fp.close()
 else:
     # load intput data
-    img = cv2.imread('data/LPlates/040603/P6040007.jpg')
+    img = cv2.imread('data/LPlates/040603/P1010001.jpg')
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     _, thresh = cv2.threshold(gray, 70, 150, cv2.THRESH_BINARY)
+
+    contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
 
     cv2.imshow('img', thresh)
     cv2.imshow('img_original', img)
